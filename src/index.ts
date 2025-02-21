7e56991cb6b660e99fdd57319aa7c4c1b94a3590
NeutronDavis <neutrondavis@gmail.com>
2025-02-21 07:19:29 +0100

    second_commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import { PORT } from "./secrets";
 import rootRouters from "./routes/index";
+var cors = require('cors')
+
 let app: Express = express();
+
+// Allow all origins (not recommended for production)
+app.use(cors());
+
+// app.use(
+//     cors({
+//       origin: "http://your-frontend.com", // Replace with your frontend URL
+//       methods: ["GET", "POST", "PUT", "DELETE"],
+//       allowedHeaders: ["Content-Type", "Authorization"],
+//     })
+//   );
 
 app.use("/api", rootRouters)
 
