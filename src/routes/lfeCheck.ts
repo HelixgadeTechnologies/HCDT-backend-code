1d0128bd09451e1880ab91dc381d97b1010db920
NeutronDavis <neutrondavis@gmail.com>
2025-03-05 21:12:42 +0100

    heroku server setup

--- src/routes/lfeCheck.ts
+++ src/routes/lfeCheck.ts
@@ -1,4 +1,4 @@
 import { Request, Response } from "express";
 export const lifeCheck = async (req: Request, res: Response) => {
-    res.send("HCDT API WORKING");
+    res.send("<h1>HCDT SERVER RUNNING IN GOOD HEALTH WITH THE DATABASE</h1>");
 };
